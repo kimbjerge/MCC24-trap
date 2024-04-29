a663d9e87117220dee8a01097c9c107513476551
Kim Bjerge <au288681@uni.au.dk>
2024-04-29 13:58:27 +0200

    Added scripts to process images from traps in 2022

---
+++ MothsPredictSpecies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+input="$1.txt"
+filepath="/mnt/Dfs/Tech_TTH-KBE/MAVJF/data/2022/$1/"
+csv=".csv"
+npy=".npy"
+while IFS= read -r line
+do
+	echo "$filepath$line"
+	python detectClassifySpecies.py --weights insectMoths-bestF1-1280m6.pt --result $1 --img 1280 --conf 0.20 --nosave --source $filepath$line
+	mv $1.csv "M2022S/$1/$line$csv"
+	mv $1.npy "M2022S/$1/$line$npy"
+done < "$input"
