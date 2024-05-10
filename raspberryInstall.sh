2a2f49e3428559138e84ee09a00fd8f7d6c43b54
Kim Bjerge <au288681@uni.au.dk>
2024-05-10 15:04:03 +0200

    Added installation on Raspberry Pi 4+5 and test

---
+++ raspberryInstall.sh
@@ -0,0 +1,12 @@
+python -m venv /home/pi/venv
+source /home/pi/venv/bin/activate
+python -m pip install opencv-python
+python -m pip install torch torchvision torchaudio
+python -m pip install pandas
+python -m pip install seaborn
+python -m pip install requests
+python -m pip install scikit-learn
+python -m pip install matplotlib
+python -m pip install timm
+python -m pip install sqlalchemy
+
